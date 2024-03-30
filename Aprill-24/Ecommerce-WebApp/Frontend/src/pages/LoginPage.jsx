2c8cc6269a9e3332074aa952ae72e143e32deab4
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-03-30 17:54:25 +0600

    UPDATE-2: Signup and Login Pages for component completed

---
+++ Aprill-24/Ecommerce-WebApp/Frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginAuth from '../features/auth/components/LoginAuth'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <LoginAuth/>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ Aprill-24/Ecommerce-WebApp/Frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupAuth from '../features/auth/components/SignupAuth'
+
+const SignupPage = () => {
+    return (
+        <div>
+            <SignupAuth />
+        </div>
+    )
+}
+
+export default SignupPage
