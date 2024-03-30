e69c3f603c750a83ba7eeb8bb4d52362f5ef8723
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-03-30 17:55:28 +0600

    UPDATE-3: Cart Component added

---
+++ Aprill-24/Ecommerce-WebApp/Frontend/src/features/cart/cartApi.js
@@ -0,0 +1,10 @@
+
+export function fetchProduct(amount = 1) {
+    return new Promise(async (resolve) => {
+
+        const response = await fetch("http://localhost:8080");
+        const data = await response.json();
+        resolve(data);
+
+    })
+}
