35d63e43bd9d958f041a05e5a53e7448504ae4a5
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-04-09 02:53:48 +0600

    UPDATE-4: Create a test API routes for user

--- Aprill-24/Blog-App-Wth-Dashboard/backend/index.js
+++ Aprill-24/Blog-App-Wth-Dashboard/backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from '../backend/routes/user.route.js'
 const app = express();
 
 dotenv.config()
@@ -10,6 +11,7 @@ mongoose.connect(
     console.log('Mongodb is connected ')
 })
 
+app.use('/api/user', userRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
