9418097593dd44a5ef3a8d9604430d7b6f887827
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-04-09 03:26:51 +0600

    UPDATE-5:Create Signup auth and data save to DB

--- Aprill-24/Blog-App-Wth-Dashboard/backend/index.js
+++ Aprill-24/Blog-App-Wth-Dashboard/backend/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-import userRoutes from '../backend/routes/user.route.js'
+import userRoutes from '../backend/routes/user.route.js';
+import authRoutes from '../backend/routes/auth.route.js'
 const app = express();
+
+app.use(express.json());
 
 dotenv.config()
 mongoose.connect(
@@ -11,7 +14,9 @@ mongoose.connect(
     console.log('Mongodb is connected ')
 })
 
+//Routes
 app.use('/api/user', userRoutes);
+app.use('/api/user', authRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
