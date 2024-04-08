6b502d4a92b6bb3f28198a7c36274845e4f6f4de
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-04-09 01:06:13 +0600

    UPDATE-1 : Create Pages and Adding the routes.

--- Aprill-24/Blog-App-Wth-Dashboard/client/src/App.jsx
+++ Aprill-24/Blog-App-Wth-Dashboard/client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import About from './pages/About'
+import SignInPage from './pages/SignInPage'
+import SignUpPage from './pages/SignUpPage'
+import DashboardPage from './pages/DashboardPage'
+import ProjectsPage from './pages/ProjectsPage'
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignInPage />} />
+        <Route path='/sign-Up' element={<SignUpPage />} />
+        <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='/projects' element={<ProjectsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
