957d1b4c4a3f5e2472b37dc315e30526220d6466
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-04-09 02:16:50 +0600

    SETTINGS-2: Connect to the mongodb Database to the server

--- Aprill-24/Blog-App-Wth-Dashboard/backend/index.js
+++ Aprill-24/Blog-App-Wth-Dashboard/backend/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 const app = express();
+
+dotenv.config()
+mongoose.connect(
+    process.env.MONGODB_URI
+).then(() => {
+    console.log('Mongodb is connected ')
+})
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
+//16ezaJQ8p3IMJMgC
