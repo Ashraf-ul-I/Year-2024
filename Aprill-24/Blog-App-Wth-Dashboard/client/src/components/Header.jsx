a8a834e50c47e68630ddbfd97fffab42eaa72957
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-04-09 02:04:48 +0600

    SETTINGS: Initialized backend server

--- Aprill-24/Blog-App-Wth-Dashboard/client/src/components/Header.jsx
+++ Aprill-24/Blog-App-Wth-Dashboard/client/src/components/Header.jsx
@@ -29,10 +29,12 @@ const Header = () => {
             <div className='flex gap-2 md:order-2'>
                 <Button className='w-12 h-10 hidden sm:inline' color='gray' pill><FaMoon /></Button>
                 <Link to='/sign-in'>
-                    <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+                    <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
                 </Link>
                 <NavbarToggle />
             </div>
+            {/* This things will woks as if we big the screen all the things will show in pages but if we in Mobile screen the inner dic navbar toggle will show in the right corner and whenever we just click on 
+            toggle this below collapse section all things will show. */}
             <Navbar.Collapse >
                 <Navbar.Link active={path === "/"} as={'div'}>
                     <Link to='/'>
