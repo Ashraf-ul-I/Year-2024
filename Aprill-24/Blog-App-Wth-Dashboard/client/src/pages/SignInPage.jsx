ba388d841a995515674488bf9ae06db40be423bb
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-04-09 07:37:32 +0600

    UPDATE-10: Complete Signin Pages functionality.

--- Aprill-24/Blog-App-Wth-Dashboard/client/src/pages/SignInPage.jsx
+++ Aprill-24/Blog-App-Wth-Dashboard/client/src/pages/SignInPage.jsx
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
 const SignInPage = () => {
+    const [formdata, setFormdata] = useState({});
+    const [errorMessage, setErrormessage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormdata({ ...formdata, [e.target.id]: e.target.value.trim() });
+
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!formdata.email || !formdata.password) {
+            return setErrormessage('Please fill out all fields');
+        }
+        try {
+            setLoading(true);
+            setErrormessage(null);
+            const res = await fetch('/api/auth/signin', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formdata)
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                return setErrormessage(data.message);
+            }
+            setLoading(false);
+            if (res.ok) {
+                navigate('/')
+            }
+        } catch (error) {
+            setErrormessage(error.message);
+            setLoading(false);
+        }
+    }
+
+
     return (
-        <div>SignInPage</div>
+        <div className='min-h-screen mt-20 '>
+            <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center'>
+                {/* LeftSide */}
+                <div className='flex-1'>
+                    <Link to="/" className=' font-bold dark:text-white text-4xl'>
+                        <span className='px-2 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Ashraful's</span>
+                        Blog
+                    </Link>
+                    <p className='text-sm mt-5'>
+                        This is a blog project for the reader.You can sign In with your email and password
+                        or with Google
+                    </p>
+                </div>
+
+                {/* rghtSide */}
+                <div className='flex-1'>
+                    <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+
+                        <div>
+                            <Label value='Your Email'></Label>
+                            <TextInput
+                                type='email'
+                                placeholder='myemail@company.com'
+                                id='email'
+                                onChange={handleChange} />
+
+                        </div>
+                        <div>
+                            <Label value='Your Password'></Label>
+                            <TextInput
+                                type='password'
+                                placeholder='*********'
+                                id='password'
+                                onChange={handleChange} />
+
+                        </div>
+                        <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>{loading ? (<Spinner size='sm'><span className='pl-3'>Loading...</span></Spinner>) : 'SignIn'}</Button>
+                    </form>
+
+                    <div className='flex gap-2 text-sm mt-5'>
+                        <span>Don't have an Account?</span>
+                        <Link to='/sign-up' className='text-blue-500'>Sign Up</Link>
+                    </div>
+                    {
+                        errorMessage && (<Alert className='mt-5' color='failure'>{errorMessage}</Alert>)
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
