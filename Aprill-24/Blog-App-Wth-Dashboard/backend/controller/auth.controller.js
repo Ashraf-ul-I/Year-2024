f49207d905a26a69b47618e762d63be1f44576ed
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-04-09 07:28:07 +0600

    UPDATE-9: Signin Backend completed

--- Aprill-24/Blog-App-Wth-Dashboard/backend/controller/auth.controller.js
+++ Aprill-24/Blog-App-Wth-Dashboard/backend/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -24,5 +25,43 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 
+
+}
+
+//SIGNIN 
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password || email === "" || password === "") {
+        next(errorHandler(400, "All fields are required"))
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return next(errorHandler(400, 'User not found'))
+        }
+
+        const validPassword = bcryptjs.compare(password, user.password);
+        if (!validPassword) {
+            return next(errorHandler(400, 'Invalid Password'))
+        }
+
+        const token = jwt.sign({
+            userId: user._id,
+            username: user.username
+        },
+            process.env.JWT_SECRET,
+            { expiresIn: '10d' }
+        );
+        const { password: pass, ...rest } = user._doc //this is uses for not getting the password after signin for security
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
 
 }
