d7426f5eb604a433e4ca530dda072bb4531ac8f2
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-04-10 16:46:22 +0600

    UPDATE-13: Complete dark mode functionality

--- Aprill-24/Blog-App-Wth-Dashboard/client/src/components/Header.jsx
+++ Aprill-24/Blog-App-Wth-Dashboard/client/src/components/Header.jsx
@@ -3,11 +3,15 @@ import { Avatar, Button, Dropdown, DropdownHeader, Navbar, NavbarToggle, TextInp
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FiSearch } from 'react-icons/fi';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector } from 'react-redux'
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice';
 const Header = () => {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
     const { currentUser } = useSelector(state => state.user);
+    const { theme } = useSelector(state => state.theme);
+
     return (
         <Navbar className='border-b-2'>
             <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -28,7 +32,12 @@ const Header = () => {
             </Button>
             {/* this div flex will make the two button stay align together  */}
             <div className='flex gap-2 md:order-2'>
-                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill><FaMoon /></Button>
+                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
+                    {
+                        theme === 'dark' ? <FaMoon /> : <FaSun />
+
+                    }
+                </Button>
                 {
                     currentUser ? (
                         <Dropdown arrowIcon={false} inline label={<Avatar alt='user' img={currentUser.profilePicture} rounded />}>
--- Aprill-24/Blog-App-Wth-Dashboard/client/src/redux/store.js
+++ Aprill-24/Blog-App-Wth-Dashboard/client/src/redux/store.js
@@ -3,9 +3,10 @@ import userReducer from './user/userSlice.js';
 import { persistReducer } from 'redux-persist';
 import persistStore from 'redux-persist/es/persistStore.js';
 import storage from 'redux-persist/lib/storage';
-
+import themeReducer from './theme/themeSlice.js'
 const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    theme: themeReducer
 })
 
 const persistConfig = {
