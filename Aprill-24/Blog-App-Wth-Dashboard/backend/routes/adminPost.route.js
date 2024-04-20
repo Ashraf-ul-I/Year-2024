63a6620ed9d3241004c5edc61a17d54934078816
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-04-20 18:18:54 +0600

    UPDATE-29: Add Delete posts functionality to the dashboard

--- Aprill-24/Blog-App-Wth-Dashboard/backend/routes/adminPost.route.js
+++ Aprill-24/Blog-App-Wth-Dashboard/backend/routes/adminPost.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { verifyToken } from '../utils/varifyUser.js';
-import { create, getposts } from '../controller/post.controller.js';
+import { create, getposts, deletePost } from '../controller/post.controller.js';
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
 router.get('/getposts', getposts);
+router.delete('/deletepost/:postId/:userId', verifyToken, deletePost)
 export default router;
