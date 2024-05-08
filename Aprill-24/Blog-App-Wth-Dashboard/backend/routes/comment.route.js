8f11cd2859f1671b7bb987259d370ee5a47222ea
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-05-08 14:32:47 +0600

    UPDATE-37: show the comment of a post

--- Aprill-24/Blog-App-Wth-Dashboard/backend/routes/comment.route.js
+++ Aprill-24/Blog-App-Wth-Dashboard/backend/routes/comment.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { verifyToken } from '../utils/varifyUser.js';
 const router = express.Router();
-import { createComment } from '../controller/comment.controller.js'
+import { createComment, getPostComments } from '../controller/comment.controller.js'
 router.post('/create', verifyToken, createComment);
-
+router.get('/getPostComments/:postId', getPostComments)
 export default router;
--- Aprill-24/Blog-App-Wth-Dashboard/client/src/components/CommentSection.jsx
+++ Aprill-24/Blog-App-Wth-Dashboard/client/src/components/CommentSection.jsx
@@ -1,13 +1,15 @@
 import { Alert, Button, TextInput, Textarea } from 'flowbite-react';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
-
+import Comment from './Comment';
 const CommentSection = ({ postId }) => {
     const { currentUser } = useSelector(state => state.user);
     const [comment, setComment] = useState('');
     const [exceededLimit, setExceededLimit] = useState(false); // State to track if comment length exceeds the limit
     const [commentError, setCommentError] = useState(null)
+    const [comments, setComments] = useState([]);
+
     const handleCommentChange = (e) => {
         const newComment = e.target.value;
         setComment(newComment);
@@ -37,13 +39,29 @@ const CommentSection = ({ postId }) => {
 
             if (res.ok) {
                 setComment('');
-                setCommentError(null)
+                setCommentError(null);
+                setComments([data,...comments])
             }
         } catch (error) {
             setCommentError(error.message)
         }
     }
 
+    useEffect(() => {
+        const getCommments = async () => {
+            try {
+                const res = await fetch(`/api/comment/getPostComments/${postId}`);
+                if (res.ok) {
+                    const data = await res.json();
+                    setComments(data);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getCommments();
+    }, [postId])
+    console.log(comments);
 
     const limitWarning = 'You exceeded the limit';
 
@@ -91,6 +109,17 @@ const CommentSection = ({ postId }) => {
                 )
 
             }
+            {comments.length === 0 ? (<p className='text-sm my-5'>No Comments Yet!</p>) : (<>
+                <div className='text-sm my-5 flex items-center  gap-1 '>
+                    <p>Comments</p>
+                    <div className='border border-gray-400 py-1 px-2 rounded-sm'> <p>{comments.length}</p></div>
+                </div>
+                {
+                    comments.map(comment => (<Comment
+                        key={comment._id}
+                        comment={comment} />))
+                }
+            </>)}
         </div>
     )
 }
